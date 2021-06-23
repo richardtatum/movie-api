e0fcccb4f9b4dbf71a35b425801137a0faa8b99a
Richard Tatum <richard.tatum@starkwood.co.uk>
2021-06-23 18:48:26 +0100

    WatchStats class for matching against stats database

---
+++ MovieApi/Models/WatchStats.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace MovieApi.Models
+{
+    public class WatchStats
+    {
+        [Name("movieId")]
+        public int MovieId { get; set; }
+        [Name("watchDurationMs")]
+        public int WatchDurationMs { get; set; }
+    }
+}
