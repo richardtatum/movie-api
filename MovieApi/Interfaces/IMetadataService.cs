f6de5bb828630134b47da158cc08f84c9ebedd1c
Richard Tatum <richard.tatum@starkwood.co.uk>
2021-06-24 12:52:05 +0100

    Add metadata service accepting post and get

---
+++ MovieApi/Interfaces/IMetadataService.cs
@@ -0,0 +1,10 @@
+﻿using MovieApi.Models;
+
+namespace MovieApi.Interfaces
+{
+    public interface IMetadataService
+    {
+        Metadata[] Get(int movieId);
+        bool Post(Metadata metadata);
+    }
+}
---
+++ MovieApi/Services/MetadataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using MovieApi.Interfaces;
+using MovieApi.Models;
+
+namespace MovieApi.Services
+{
+    public class MetadataService : IMetadataService
+    {
+        private readonly IQueryRepository _query;
+        private readonly ICommandRepository _command;
+
+        public MetadataService(IQueryRepository query, ICommandRepository command)
+        {
+            _query = query;
+            _command = command;
+        }
+
+        public Metadata[] Get(int movieId)
+        {
+            var allMetadata = _query.GetMetadata();
+            var movies = Filter(allMetadata, movieId);
+            
+            if (!movies.Any()) throw new Exception("No movies found by that Id");
+            return movies;
+        }
+
+        public bool Post(Metadata metadata)
+        {
+            return _command.Insert(metadata);
+        }
+
+        // Filter the metadata by movieId, grouping by language, selecting the highest id and ordering by language 
+        private static Metadata[] Filter(IEnumerable<Metadata> metadata, int movieId)
+        {
+            return metadata
+                .Where(m => m.MovieId == movieId)
+                .GroupBy(m => m.Language, m => m)
+                .Select(g => g.OrderByDescending(m => m.Id).FirstOrDefault())
+                .OrderBy(m => m?.Language)
+                .ToArray();
+        }
+    }
+}
